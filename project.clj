64c9571d6fb33860b8079f06e2db54b02d779b41
ayato_p <ayato.skycrawlers@gmail.com>
2019-01-31 00:01:16 +0900

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.panchromatic/bulkhead "0.1.0"
+(defproject org.panchromatic/bulkhead "0.1.1-SNAPSHOT"
   :description "Testing library for Clojure and Integrant application"
   :url "https://github.com/ayato-p/bulkhead"
   :license {:name "MIT License"
